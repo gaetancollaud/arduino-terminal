43057c4f7d7f272ab58b45f897569d87dc8d9b01
gaetancollaud <gaetancollaud@gmail.com>
2015-10-22 20:00:25 +0200

    comments

--- SimpleTerminal.h
+++ SimpleTerminal.h
@@ -40,20 +40,71 @@ public:
 	 * Constructor.
 	 * Create the arrays for the variables and the commands.
 	 * 
-     * @param stream stream to listen to
-     * @param maxCmds size of the commands array
-     * @param maxVars size of the variables array
-     */
-	SimpleTerminal(Stream *stream, int maxCmds=5, int maxVars=5);
-	~SimpleTerminal();
+	 * @param stream stream to listen to
+	 * @param maxCmds size of the commands array
+	 * @param maxVars size of the variables array
+	 */
+	SimpleTerminal(Stream *stream, int maxCmds = 5, int maxVars = 5);
 	
+	/**
+	 * Destructor
+     */
+	~SimpleTerminal();
+
+	/**
+	 * Add a variable
+     * @param name name of the variable (must be unique)
+     * @param type the type of the variable (see VarType enum)
+     * @param ptr pointer to the variable
+     * @param fnPtr (optional) pointer to the callback invoked when the variables changed
+     * @return true if the action succeed (false if there is no more space for variables)
+     */
 	bool addVar(String name, VarType type, void *ptr = 0, const void *fnPtr = 0);
+	
+	/**
+	 * Add a command
+     * @param name name of the command (must be unique)
+     * @param fnPtr pointer to the callback of the command
+     * @param description (optional) description of the command (will be printed to help)
+     * @return 
+     */
 	bool addCommand(String name, const void *fnPtr, String description = "");
-	void setVarCmdEnabled(bool newValue);
-	void setConfirmPrintVar(bool newValue);
+	
+	/**
+	 * Enable or disable all variables (default is true, enabled)
+     * @param newValue
+     */
+	void setVarCmdEnabled(bool enabled);
+	
+	/**
+	 * Enable or disable the confirmation when a variable is set (default is true, enabled)
+     * @param newValue
+     */
+	void setConfirmPrintVar(bool enabled);
+	
+	/**
+	 * Run the system.
+	 * 
+	 * Should be invoked every Arduino loop for fast response or some times if time is not 
+	 * critical (do not wait to much to not overflow the Stream buffer)
+     */
 	void run();
+	
+	/**
+	 * Programatically print help (equivalent of typing 'help' in the terminal)
+     */
 	void printHelp();
+	
+	/**
+	 * Print the variables (equivalent of typing 'printenv' in the terminal)
+     */
 	void printEnv();
+	
+	/**
+	 * Redefine the stream. Normally you should use the constructor parameter but sometimes
+	 * you need to define the stream afterward.
+     * @param stream
+     */
 	void setStream(Stream *stream);
 
 private:
@@ -65,7 +116,7 @@ private:
 	void commandSet(String &line);
 	void commandGet(String &line);
 	void printVar(RegVar *var);
-	
+
 	bool varCmdEnabled = true;
 	bool confirmPrintVar = true;
 
