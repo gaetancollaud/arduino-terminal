35704593b4892eebf1a72daf9ac7d38051a8cc91
gaetancollaud <gaetancollaud@gmail.com>
2015-10-22 20:02:14 +0200

    formating

--- SimpleTerminal.h
+++ SimpleTerminal.h
@@ -45,66 +45,66 @@ public:
 	 * @param maxVars size of the variables array
 	 */
 	SimpleTerminal(Stream *stream, int maxCmds = 5, int maxVars = 5);
-	
+
 	/**
 	 * Destructor
-     */
+	 */
 	~SimpleTerminal();
 
 	/**
 	 * Add a variable
-     * @param name name of the variable (must be unique)
-     * @param type the type of the variable (see VarType enum)
-     * @param ptr pointer to the variable
-     * @param fnPtr (optional) pointer to the callback invoked when the variables changed
-     * @return true if the action succeed (false if there is no more space for variables)
-     */
+	 * @param name name of the variable (must be unique)
+	 * @param type the type of the variable (see VarType enum)
+	 * @param ptr pointer to the variable
+	 * @param fnPtr (optional) pointer to the callback invoked when the variables changed
+	 * @return true if the action succeed (false if there is no more space for variables)
+	 */
 	bool addVar(String name, VarType type, void *ptr = 0, const void *fnPtr = 0);
-	
+
 	/**
 	 * Add a command
-     * @param name name of the command (must be unique)
-     * @param fnPtr pointer to the callback of the command
-     * @param description (optional) description of the command (will be printed to help)
-     * @return 
-     */
+	 * @param name name of the command (must be unique)
+	 * @param fnPtr pointer to the callback of the command
+	 * @param description (optional) description of the command (will be printed to help)
+	 * @return 
+	 */
 	bool addCommand(String name, const void *fnPtr, String description = "");
-	
+
 	/**
 	 * Enable or disable all variables (default is true, enabled)
-     * @param newValue
-     */
+	 * @param newValue
+	 */
 	void setVarCmdEnabled(bool enabled);
-	
+
 	/**
 	 * Enable or disable the confirmation when a variable is set (default is true, enabled)
-     * @param newValue
-     */
+	 * @param newValue
+	 */
 	void setConfirmPrintVar(bool enabled);
-	
+
 	/**
 	 * Run the system.
 	 * 
 	 * Should be invoked every Arduino loop for fast response or some times if time is not 
 	 * critical (do not wait to much to not overflow the Stream buffer)
-     */
+	 */
 	void run();
-	
+
 	/**
 	 * Programatically print help (equivalent of typing 'help' in the terminal)
-     */
+	 */
 	void printHelp();
-	
+
 	/**
 	 * Print the variables (equivalent of typing 'printenv' in the terminal)
-     */
+	 */
 	void printEnv();
-	
+
 	/**
 	 * Redefine the stream. Normally you should use the constructor parameter but sometimes
 	 * you need to define the stream afterward.
-     * @param stream
-     */
+	 * @param stream
+	 */
 	void setStream(Stream *stream);
 
 private:
